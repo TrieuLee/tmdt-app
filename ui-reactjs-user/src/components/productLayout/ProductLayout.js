f667947993560a4e958ccfdcefd8be42f277fdda
ScofieldP <thienphuc.luongtran@gmail.com>
2022-10-13 11:41:16 +0700

    mui

--- ui-reactjs-user/src/components/productLayout/ProductLayout.js
+++ ui-reactjs-user/src/components/productLayout/ProductLayout.js
@@ -1,13 +1,35 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import { useParams } from "react-router-dom";
+import Grid from '@mui/material/Grid'; // Grid version 1
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Container from '@mui/material/Container'
 export default function ProductLayout() {
   const { id } = useParams();
   return (
     <>
       <Navbar />
-      
-      <div>Day la {id} </div>
+      <Container>
+        <div>Day la {id} </div>
+
+        <Grid
+          container spacing={2}
+          border={1}>
+          <Grid item xs={8} border={1}>
+            Alo xs=8
+          </Grid>
+          <Grid item xs={4} border={1}>
+            xs=4
+          </Grid>
+          <Grid item xs={4}>
+            xs=4
+          </Grid>
+          <Grid item xs={8}>
+            xs=8
+          </Grid>
+        </Grid>
+      </Container>
+
     </>
   );
 }
