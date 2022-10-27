ca67012b443795c588f6cd9fdf4b21b97deb67d2
ScofieldP <thienphuc.luongtran@gmail.com>
2022-10-27 23:56:32 +0700

    layoutlogin

--- ui-reactjs-user/src/pages/login/Login.jsx
+++ ui-reactjs-user/src/pages/login/Login.jsx
@@ -1,41 +1,30 @@
-import React from "react";
+import React from 'react';
 // import component
 //import style
-import "./login.scss";
+import './login.scss';
 
 export default function Login() {
-  return (
-    <div className="login">
-      <div className="loginWrapper">
-        <div className="loginLeft">
-          <h3 className="loginLogo">.Sneaker</h3>
-          <span className="loginDesc">By your Fashion and Enjoy yourself</span>
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+    return (
+        <div className="login" style={{ backgroundImage: 'url(' + PF + 'img/5482397.jpg)', backgroundSize: 'cover' }}>
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">.Sneaker</h3>
+                    <span className="loginDesc" style={{color: 'white'}}>By your Fashion and Enjoy yourself</span>
+                </div>
+                <div className="loginRight">
+                    <form className="loginBox">
+                        <input placeholder="Email" type="email" className="loginInput" required />
+                        <input placeholder="Mật khẩu" type="password" className="loginInput" required minLength="8" />
+                        <button className="loginButon" type="submit">
+                            Login
+                        </button>
+                        <span className="loginForgot">Forgot password?</span>
+                        <button className="loginRegisterButton">Register new account</button>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className="loginRight">
-          <form className="loginBox">
-            <input
-              placeholder="Email"
-              type="email"
-              className="loginInput"
-              required
-            />
-            <input
-              placeholder="Mật khẩu"
-              type="password"
-              className="loginInput"
-              required
-              minLength="8"
-            />
-            <button className="loginButon" type="submit">
-              Login
-            </button>
-            <span className="loginForgot">Forgot password?</span>
-            <button className="loginRegisterButton">
-              Register new account
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
