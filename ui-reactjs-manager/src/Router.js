2e2bfcc8fd07ef78a8139f7b27863122be5e5119
ScofieldP <thienphuc.luongtran@gmail.com>
2022-10-28 23:18:37 +0700

    admin templete

--- ui-reactjs-manager/src/Router.js
+++ ui-reactjs-manager/src/Router.js
@@ -2,13 +2,29 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 //component
-import Sidebar from './components/sidebar/Sidebar';
+import List from './pages/list/List';
+import Single from './pages/single/Single';
+import { productInputs, userInputs } from './formSource';
+import New from './pages/new/New';
+
 import Home from './pages/home/Home';
 const Router = () => {
     return (
         <>
             <Routes>
-                <Route exect path="/" element={<Home />} />
+                <Route path="/">
+                    <Route index element={<Home />} />
+                    <Route path="users">
+                        <Route index element={<List />} />
+                        <Route path=":userId" element={<Single />} />
+                        <Route path="new" element={<New inputs={userInputs} title="Add New User" />} />
+                    </Route>
+                    <Route path="products">
+                        <Route index element={<List />} />
+                        <Route path=":productId" element={<Single />} />
+                        <Route path="new" element={<New inputs={productInputs} title="Add New Product" />} />
+                    </Route>
+                </Route>
             </Routes>
         </>
     );
