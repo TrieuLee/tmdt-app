0634ecd5ad7ac26769ed1488dce0b180607a4b17
ScofieldP <thienphuc.luongtran@gmail.com>
2022-10-29 23:12:43 +0700

    new edit

--- ui-reactjs-manager/src/pages/product/editProduct/editProduct.jsx
+++ ui-reactjs-manager/src/pages/product/editProduct/editProduct.jsx
@@ -13,7 +13,7 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-export default function E ditProduct({ title }) {
+export default function EditProduct({ title }) {
   // Hãng
   const [brand, setBrand] = useState("");
   const changeBrand = (event) => {
