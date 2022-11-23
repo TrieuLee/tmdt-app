ae9903c5b0c16eaa09c7cbe8b020cf8165d71565
ScofieldP <thienphuc.luongtran@gmail.com>
2022-11-23 16:58:34 +0700

    asd

--- ui-reactjs-user/src/callAPIs.js
+++ ui-reactjs-user/src/callAPIs.js
@@ -3,7 +3,10 @@ import axios from "axios";
 export const loginCall = async (userCredentials, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("users/login", userCredentials);
+    const res = await axios.post(
+      "https://sneaker-huflit-server.herokuapp.com/api/users/login",
+      userCredentials
+    );
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
     dispatch({ type: "LOGIN_FAILURE", payload: err });
