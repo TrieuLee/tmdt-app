93ba04e59a8a4394ac7c56a91df60a39763bd31b
ScofieldP <thienphuc.luongtran@gmail.com>
2022-11-27 22:15:17 +0700

    still fix cart

--- ui-reactjs-user/src/components/cart/Cart.jsx
+++ ui-reactjs-user/src/components/cart/Cart.jsx
@@ -1,134 +1,21 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import SlidingPane from "react-sliding-pane";
-import "react-sliding-pane/dist/react-sliding-pane.css";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import { IconButton } from "@mui/material";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
-import { AuthContext } from "../../context/AuthContext";
+// import React, { useContext } from "react";
+// import { Link } from "react-router-dom";
+// import SlidingPane from "react-sliding-pane";
+// import "react-sliding-pane/dist/react-sliding-pane.css";
+// import Button from "@mui/material/Button";
+// import Grid from "@mui/material/Grid";
+// import { IconButton } from "@mui/material";
+// import AddCircleIcon from "@mui/icons-material/AddCircle";
+// import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+// import { AuthContext } from "../../context/AuthContext";
 
-function Cart(props) {
-  const { user } = useContext(AuthContext);
+// function Cart(props) {
+//   const { user } = useContext(AuthContext);
 
-  const { visible, onRequestClose } = props;
-  const { onAdd, onRemove } = props;
-  const { cartItems } = { props };
-  console.log(cartItems);
-  const itemsPrice = cartItems
-    ? cartItems.reduce((a, c) => a + c.price * c.qty, 0)
-    : 0;
-  const dis = itemsPrice * 0.01;
-  const shippingPrice = itemsPrice < 200000 ? 0 : dis;
-  const totalPrice = itemsPrice + shippingPrice;
-  function SetCartPayment() {
-    const lstOrFd = {
-      cart: cartItems,
-      itemsPrice: itemsPrice ? itemsPrice : "",
-      shippingPrice: shippingPrice ? shippingPrice : "",
-      totalPrice: totalPrice ? totalPrice : "",
-    };
-    localStorage.setItem("lstOrFd", JSON.stringify(lstOrFd));
-  }
-  const product = localStorage.getItem("products");
 
-  return (
-    <SlidingPane
-      className="some-custom-class"
-      isOpen={visible}
-      title="Giỏ hàng của tôi"
-      width="40%"
-      onRequestClose={onRequestClose}
-      cartItems={cartItems}
-    >
-      {cartItems && cartItems.length === 0 && <p>Giỏ hàng trống</p>}
-      {cartItems &&
-        cartItems.map((item) => (
-          <Grid container key={item._id}>
-            <Grid item xs={3}>
-              <img src={item.images} style={{ width: "100px" }} alt="" />
-            </Grid>
-            <Grid item xs={6} sx={{ display: "flex", alignItems: "center" }}>
-              <ul style={{ listStyle: "none", padding: "0", margin: "0" }}>
-                <li>
-                  <b>{item.title}</b>
-                </li>
-                <li style={{ marginTop: "8px" }}>
-                  <b>
-                    {item.price.toLocaleString("it-IT", {
-                      style: "currency",
-                      currency: "VND",
-                    })}
-                  </b>
-                </li>
-                <li style={{ marginTop: "8px" }}>
-                  <b>Size:</b> {item.size}
-                </li>
-                <li style={{ marginTop: "8px" }}>
-                  <b>Số lượng:</b> {item.qty} x{" "}
-                  {item.price.toLocaleString("it-IT", {
-                    style: "currency",
-                    currency: "VND",
-                  })}
-                </li>
-              </ul>
-            </Grid>
-            <Grid item xs={3} sx={{ display: "flex", alignItems: "center" }}>
-              <IconButton onClick={() => onAdd(item)}>
-                <AddCircleIcon />
-              </IconButton>
+//   const product = localStorage.getItem("products");
 
-              <IconButton onClick={() => onRemove(item)}>
-                <RemoveCircleIcon />
-              </IconButton>
-            </Grid>
-          </Grid>
-        ))}
-      {cartItems && cartItems.length !== 0 && (
-        <>
-          <div style={{ marginTop: "70%" }}>
-            <p style={{ display: "flex", justifyContent: "end" }}>
-              Tạm tính:{" "}
-              {itemsPrice.toLocaleString("it-IT", {
-                style: "currency",
-                currency: "VND",
-              })}
-            </p>
-            <p style={{ display: "flex", justifyContent: "end" }}>
-              Phí ship:{" "}
-              {shippingPrice.toLocaleString("it-IT", {
-                style: "currency",
-                currency: "VND",
-              })}
-            </p>
-            <p style={{ display: "flex", justifyContent: "end" }}>
-              Tổng tiền:{" "}
-              {totalPrice.toLocaleString("it-IT", {
-                style: "currency",
-                currency: "VND",
-              })}
-            </p>
-          </div>
-          {user ? (
-            <Link
-              type="submit"
-              value="Thanh toán"
-              to="/checkout"
-              style={{ textDecoration: "none", color: "white" }}
-              onClick={SetCartPayment}
-            >
-              <Button variant="contained" sx={{ width: "100%" }}>
-                Thanh Toán
-              </Button>
-            </Link>
-          ) : (
-            <p>Hãy đăng nhập để thanh toán sản phẩm</p>
-          )}
-        </>
-      )}
-    </SlidingPane>
-  );
-}
+//   return <div>gio</div>;
+// }
 
-export default Cart;
+// export default Cart;
--- ui-reactjs-user/src/components/navbar/Navbar.jsx
+++ ui-reactjs-user/src/components/navbar/Navbar.jsx
@@ -8,10 +8,12 @@ import { styled } from "@mui/system";
 import { IconButton } from "@mui/material";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import Cart from "../cart/Cart";
-
+import SlidingPane from "react-sliding-pane";
 import { AuthContext } from "../../context/AuthContext";
-
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 export default function Navbar(props) {
   const { user } = useContext(AuthContext);
 
@@ -38,6 +40,22 @@ export default function Navbar(props) {
     setSearch(searchTerm);
     console.log("search", searchTerm);
   };
+
+  const itemsPrice = cartItems
+    ? cartItems.reduce((a, c) => a + c.price * c.qty, 0)
+    : 0;
+  const dis = itemsPrice * 0.01;
+  const shippingPrice = itemsPrice < 200000 ? 0 : dis;
+  const totalPrice = itemsPrice + shippingPrice;
+  function SetCartPayment() {
+    const lstOrFd = {
+      cart: cartItems,
+      itemsPrice: itemsPrice ? itemsPrice : "",
+      shippingPrice: shippingPrice ? shippingPrice : "",
+      totalPrice: totalPrice ? totalPrice : "",
+    };
+    localStorage.setItem("lstOrFd", JSON.stringify(lstOrFd));
+  }
   return (
     <>
       <nav className="nav">
@@ -128,16 +146,121 @@ export default function Navbar(props) {
           <div className="navItem">
             <IconButton onClick={() => setIsLiked(!isLiked)}>
               <ThemeComponent color="error" sx={{ mb: 1 }} fontSize="large" />
-              <Cart
-                visible={isLiked}
-                cartItems={cartItems}
-                onAdd={onAdd}
-                onRemove={onRemove}
-                onRequestClose={() => {
-                  setIsLiked(isLiked);
-                }}
-              />
             </IconButton>
+
+            <SlidingPane
+              className="some-custom-class"
+              isOpen={isLiked}
+              title="Giỏ hàng của tôi"
+              width="40%"
+              onRequestClose={() => {
+                setIsLiked(!isLiked);
+              }}
+            >
+              {cartItems && cartItems.length === 0 && <p>Giỏ hàng trống</p>}
+              {cartItems &&
+                cartItems.map((item) => (
+                  <Grid container key={item._id}>
+                    <Grid item xs={3}>
+                      <img
+                        src={item.images}
+                        style={{ width: "100px" }}
+                        alt=""
+                      />
+                    </Grid>
+                    <Grid
+                      item
+                      xs={6}
+                      sx={{ display: "flex", alignItems: "center" }}
+                    >
+                      <ul
+                        style={{
+                          listStyle: "none",
+                          padding: "0",
+                          margin: "0",
+                        }}
+                      >
+                        <li>
+                          <b>{item.title}</b>
+                        </li>
+                        <li style={{ marginTop: "8px" }}>
+                          <b>
+                            {item.price.toLocaleString("it-IT", {
+                              style: "currency",
+                              currency: "VND",
+                            })}
+                          </b>
+                        </li>
+                        <li style={{ marginTop: "8px" }}>
+                          <b>Size:</b> {item.size}
+                        </li>
+                        <li style={{ marginTop: "8px" }}>
+                          <b>Số lượng:</b> {item.qty} x{" "}
+                          {item.price.toLocaleString("it-IT", {
+                            style: "currency",
+                            currency: "VND",
+                          })}
+                        </li>
+                      </ul>
+                    </Grid>
+                    <Grid
+                      item
+                      xs={3}
+                      sx={{ display: "flex", alignItems: "center" }}
+                    >
+                      <IconButton onClick={() => onAdd(item)}>
+                        <AddCircleIcon />
+                      </IconButton>
+
+                      <IconButton onClick={() => onRemove(item)}>
+                        <RemoveCircleIcon />
+                      </IconButton>
+                    </Grid>
+                  </Grid>
+                ))}
+              {cartItems && cartItems.length !== 0 && (
+                <>
+                  <div style={{ marginTop: "70%" }}>
+                    <p style={{ display: "flex", justifyContent: "end" }}>
+                      Tạm tính:{" "}
+                      {itemsPrice.toLocaleString("it-IT", {
+                        style: "currency",
+                        currency: "VND",
+                      })}
+                    </p>
+                    <p style={{ display: "flex", justifyContent: "end" }}>
+                      Phí ship:{" "}
+                      {shippingPrice.toLocaleString("it-IT", {
+                        style: "currency",
+                        currency: "VND",
+                      })}
+                    </p>
+                    <p style={{ display: "flex", justifyContent: "end" }}>
+                      Tổng tiền:{" "}
+                      {totalPrice.toLocaleString("it-IT", {
+                        style: "currency",
+                        currency: "VND",
+                      })}
+                    </p>
+                  </div>
+                  {user ? (
+                    <Link
+                      type="submit"
+                      value="Thanh toán"
+                      to="/checkout"
+                      style={{ textDecoration: "none", color: "white" }}
+                      onClick={SetCartPayment}
+                    >
+                      <Button variant="contained" sx={{ width: "100%" }}>
+                        Thanh Toán
+                      </Button>
+                    </Link>
+                  ) : (
+                    <p>Hãy đăng nhập để thanh toán sản phẩm</p>
+                  )}
+                </>
+              )}
+            </SlidingPane>
 
             {user == null ? (
               <Link to="/login">
