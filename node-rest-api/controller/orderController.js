113d91498f53d901f139c7dc2f540ea9a8881574
ScofieldP <thienphuc.luongtran@gmail.com>
2022-12-01 11:11:56 +0700

    order model

---
+++ node-rest-api/controller/orderController.js
@@ -0,0 +1,60 @@
+const { Order } = require("../models");
+
+class OrderCRUD {
+  // CREATE
+  async create(req, res) {
+    const newOrder = new Order(req.body);
+
+    try {
+      const savedOrder = await newOrder.save();
+      res.status(200).json(savedOrder);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+//   UPDATE
+async update(req, res) {
+    try {
+        const updatedOrder = await Order.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedOrder);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+  }
+// DELETE
+async delete(req, res) {
+    try {
+        await Order.findByIdAndDelete(req.params.id);
+        res.status(200).json("Đơn hàng đã được xoá");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+  }
+// GET USER ORDERS
+  async getUserOrder(req, res) {
+    try {
+        const orders = await Order.find({ userId: req.params.userId });
+        res.status(200).json(orders);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+  }
+
+//   GET ALL ORDERS
+  async get(req, res){
+    try {
+        const orders = await Order.find();
+        res.status(200).json(orders);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+  }
+}
+module.exports = new OrderCRUD();
--- node-rest-api/models/Order.js
+++ node-rest-api/models/Order.js
@@ -1,44 +1,22 @@
 const mongoose = require("mongoose");
-const { Sequelize, DataTypes } = require("sequelize");
 
 const OrderSchema = new mongoose.Schema(
   {
-    userID: {
-      type: String,
-      required: true,
-    },
-    addressDeli: {
-      type: String,
-      require: true,
-    },
-    total: {
-      type: Number,
-      default: 0,
-    },
-    amount: {
-      type: Number,
-      required: true,
-    },
-    payment: {
-      type: String,
-      enum: ["Thanh toán bằng tiền mặt", "Thanh toán bằng Stripe"],
-      default: "Thanh toán bằng tiền mặt",
-    },
-
-    orderDate: {
-      type: String,
-      require: true,
-    },
-    orderPayDate: {
-      type: String,
-      require: true,
-    },
-    orderStatus: {
-      type: Number,
-      // 0: hủy đơn, 1:đặt hàng thành công, 2:đã duyệt đơn, 3:đang giao, 4: giao thành công
-      enum: [0, 1, 2, 3, 4],
-      default: 0,
-    },
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    amount: { type: Number, required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: "pending" },
   },
   { timestamps: true }
 );
