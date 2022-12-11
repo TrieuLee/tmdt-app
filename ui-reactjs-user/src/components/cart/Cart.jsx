69d5c58eadfc02f7799d0dc2e1bf3440878d6d6e
ScofieldP <thienphuc.luongtran@gmail.com>
2022-12-11 22:13:43 +0700

    ship again

--- ui-reactjs-user/src/components/cart/Cart.jsx
+++ ui-reactjs-user/src/components/cart/Cart.jsx
@@ -40,8 +40,8 @@ export default function Cart(props) {
       const x = item.img;
       totalQuantity += item.quantity;
       subPrice += item.price * item.quantity;
-      shipPrice = 20;
-      totalPrice = subPrice < 100 ? subPrice + 20 : 0;
+      shipPrice = subPrice < 100 ? "20" : "0";
+      totalPrice = subPrice < 100 ? subPrice + 20 : subPrice;
     });
     return { subPrice, totalPrice, totalQuantity, shipPrice };
   };
