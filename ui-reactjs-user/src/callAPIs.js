9b9460931a6292cfd42aec80b54896c4ab96114b
TrieuLee <lehaitrieu48@gmail.com>
2022-12-13 12:58:03 +0700

    1

--- ui-reactjs-user/src/callAPIs.js
+++ ui-reactjs-user/src/callAPIs.js
@@ -4,7 +4,7 @@ export const loginCall = async (userCredentials, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
     const res = await axios.post(`${domain}/api/auth/login`, userCredentials);
-
+    
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
     dispatch({ type: "LOGIN_FAILURE", payload: err });
